23b39f625372fd26fc0d7b2ad78464c859fefc2d
edison <edison@example.com>
2020-01-10 10:03:19 +0800

    updated scripts and added terraform

--- ansible/onprem/k8s/centos8/ssh-pks.sh
+++ ansible/onprem/k8s/centos8/ssh-pks.sh
@@ -3,7 +3,9 @@
 #cat ~/.ssh/id_rsa.pub | ssh root@192.168.100.201 "mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys"
 #cat ~/.ssh/id_rsa.pub | ssh root@192.168.100.202 "mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys"
 #cat ~/.ssh/id_rsa.pub | ssh root@192.168.100.197 "mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys"
-ssh-copy-id root@192.168.101.26
-
-#ansible all -m user -a "name=centos password=centos"
-#ansible-playbook add_users.yml -i hosts
+ssh-copy-id root@192.168.101.40
+ssh-copy-id root@192.168.101.41
+ssh-copy-id root@192.168.101.42
+ssh-copy-id root@192.168.101.43
+ssh-copy-id root@192.168.101.45
+ssh-copy-id root@192.168.101.46
